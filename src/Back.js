67b0d3228fe55420b5ca3afe613c68621fb12b76
Njoku Godswill Uchendu Uchechi <godswilluchendu7@gmail.com>
2022-06-28 07:27:38 -0700

    add  'My first react practice file'

---
+++ src/Back.js
@@ -0,0 +1,12 @@
+const Back = () => {
+    return (
+        <div>
+            <ul>
+                <li>NODE JS</li>
+                <li>MONGO DB</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Back;
---
+++ src/Front.js
@@ -0,0 +1,14 @@
+const Front = () =>{
+    return (
+        <div>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JS</li>
+                <li>REACT</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Front
